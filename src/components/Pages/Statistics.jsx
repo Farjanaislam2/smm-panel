544bc571a6e9370a7d56936fc7854f1ce38ba8e5
Farjanaislam2 <farjanaanika2gmail.com>
2023-10-25 14:38:23 +0600

    third section

--- src/components/Pages/Statistics.jsx
+++ src/components/Pages/Statistics.jsx
@@ -1,24 +1,46 @@
 
 
 export default function Statistics() {
-  return (
-    <div className="bg-gradient-to-r from-[#173f32]  to-[#161429] p-[90px]">
-        <div className="bg-[#222242] rounded-md p-10">
-<h1 className="text-white text-[48px] font-[700]">Statistics Of Geosmm</h1>
-<p className="text-white text-[20px] mt-14">Geosmm clients exist all over the world and we have over 50 million active clients with over 5000 active services. Our clients work with us because of the quality we offer. Growfollows offers the cheapest services in the world, and at the same time, our team is always working hard to maintain their quality so that you can get long term results.</p>
+    return (
+        <div className="bg-gradient-to-r from-[#173f32]  to-[#161429] p-[90px] ">
+         
 
-<button className="text-black text-[20px] font-bold bg-[#00FFA3] mt-12">Get Started Now</button>
-        </div>
-        <div>
-            <div className="bg-[#161429] flex gap-5 ">
-<img src="https://cdn.mypanel.link/770smr/xrgh987t2rp28cqf.webp" alt="purple bag" />
-<div className="text-white mt-4">
-    <p >
-Prices Starting From</p>
-<p>0.001$/1k</p>
-</div>
+           <div className="bg-[#222242] flex  gap-[150px]  rounded-lg p-10">
+               <div>
+               <h1 className="text-white text-[48px] font-[700]">Statistics Of Geosmm</h1>
+                <p className="text-white text-[20px] mt-14 w-[700px]">Geosmm clients exist all over the world and we have over 50 million active clients with over 5000 active services. Our clients work with us because of the quality we offer. Growfollows offers the cheapest services in the world, and at the same time, our team is always working hard to maintain their quality so that you can get long term results.</p>
+
+                <button className="text-black text-[20px] font-bold bg-[#00FFA3] mt-12">Get Started Now</button>
+               </div>
+               <div className="">
+                <div className="bg-[#161429] flex gap-5 p-7 rounded-lg">
+                    <img src="https://cdn.mypanel.link/770smr/xrgh987t2rp28cqf.webp" alt="purple bag" />
+                    <div className="text-white ">
+                        <p className="text-[18px]">
+                            Prices Starting From</p>
+                        <p className="text-[22px]">0.001$/1k</p>
+                    </div>
+                </div>
+                <div className="bg-[#161429] flex gap-5 p-5 rounded-lg mt-5">
+                    <img src="https://cdn.mypanel.link/770smr/opqq84bcorp4cyeq.webp" />
+                    <div className="text-white">
+                        <p className="text-[18px]">
+                            Prices Starting From</p>
+                        <p className="text-[22px]">0.001$/1k</p>
+                    </div>
+                </div>
+                <div className="bg-[#161429] flex gap-5 p-8 rounded-lg mt-5">
+                    <img src="https://cdn.mypanel.link/770smr/eieuwouwkem5f1o5.png" />
+                    <div className="text-white ">
+                        <p className="text-[18px]">
+                            Prices Starting From</p>
+                        <p className="text-[22px]">0.001$/1k</p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  )
+            </div>
+            
+           </div>
+        
+    )
 }
